d09475fa19e62b41e43da99c81cf8da37c3486a1
Srujan Gurram <srujangs8@gmail.com>
2023-03-06 04:58:32 +0530

    adds rainbow kit to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { RainbowConfigWrapper } from "~/components/layout/RainbowConfigWrapper";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <RainbowConfigWrapper>
+      <Component {...pageProps} />
+    </RainbowConfigWrapper>
+  );
 };
 
 export default api.withTRPC(MyApp);
