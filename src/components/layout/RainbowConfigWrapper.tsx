39a15257d67422f0e2d78fb3b7b573c164d90e84
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-06 08:46:03 +0100

    fixes UI

--- src/components/layout/RainbowConfigWrapper.tsx
+++ src/components/layout/RainbowConfigWrapper.tsx
@@ -14,7 +14,7 @@ const { chains, provider } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
+  appName: "Crypto Currents",
   chains,
 });
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 import { RainbowConfigWrapper } from "~/components/layout/RainbowConfigWrapper";
 
