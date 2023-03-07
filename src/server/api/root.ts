21a0a5d1bf20865815c01ebdcdc6d75ca1604ff6
Srujan Gurram <srujangs8@gmail.com>
2023-03-07 18:39:34 +0530

    adds userRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { tokenRouter } from "~/server/api/routers/token";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { tokenRouter } from "~/server/api/routers/token";
  */
 export const appRouter = createTRPCRouter({
   token: tokenRouter,
+  user: userRouter,
 });
 
 // export type definition of API
