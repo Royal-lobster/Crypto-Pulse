cdb2949a25cea32346e1a0500083a6cd8bab6882
Srujan Gurram <srujangs8@gmail.com>
2023-03-17 23:13:10 +0530

    Refactor getPastDayNews to fetch and scrape news articles for multiple coins and generate summaries using AI in parallel

--- src/modules/getPastDayNews.ts
+++ src/modules/getPastDayNews.ts
@@ -1,73 +1,82 @@
-import axios, { type AxiosResponse } from "axios";
-import { type ResponseData as CryptoPanicResponseData } from "~/types/cryptopanic";
-import { type ResponseData as BananaResponseData } from "~/types/banana";
 import { extract } from "@extractus/article-extractor";
+import axios, { type AxiosResponse } from "axios";
 import { env } from "~/env.mjs";
+import { type ResponseData as CryptoPanicResponseData } from "~/types/cryptopanic";
 
 export const getPastDayNews = async (coinTickers: string[], days: number) => {
-  const coinIdsString = coinTickers.join(",");
-
-  const newsRes: AxiosResponse<CryptoPanicResponseData> = await axios.get(
-    `https://cryptopanic.com/api/v1/posts/?auth_token=${env.CRYPTOPANIC_API_KEY}&currencies=${coinIdsString}&public=true&filter=hot&period=${days}d&kind=news`
+  const fetchedNewsArticles = await fetchCryptoPanicArticles(coinTickers, days);
+  const scrappedArticles = await getScrappedArticles(
+    coinTickers,
+    fetchedNewsArticles
   );
+  return scrappedArticles.filter(isTruthy);
+};
 
-  if (!newsRes.data.results) return;
+export function isTruthy<T>(value?: T | undefined | null | false): value is T {
+  return !!value;
+}
 
-  const articles = [];
-
-  for (const result of newsRes.data.results) {
+const fetchCryptoPanicArticles = async (
+  coinTickers: string[],
+  days: number
+) => {
+  const fetchedNewsArticles = new Map<
+    string,
+    CryptoPanicResponseData["results"]
+  >();
+  for (const ticker of coinTickers) {
     try {
+      const newsRes: AxiosResponse<CryptoPanicResponseData> = await axios.get(
+        `https://cryptopanic.com/api/v1/posts/?auth_token=${env.CRYPTOPANIC_API_KEY}&currencies=${ticker}&public=true&filter=hot&period=${days}d&kind=news`
+      );
+      const topNews = newsRes.data.results?.slice(0, 6);
+      fetchedNewsArticles.set(ticker, topNews);
+    } catch (e) {
+      console.error(`❌ cryptopanic fetch fail for ${ticker}`);
+    }
+  }
+  return fetchedNewsArticles;
+};
+
+const scrapArticle = async (url: string) => {
+  try {
+    return await extract(url);
+  } catch (e) {
+    console.error(`❌ scrapping fail ${url}`);
+    return null;
+  }
+};
+
+const getScrappedArticles = async (
+  coinTickers: string[],
+  fetchedNewsArticles: Map<string, CryptoPanicResponseData["results"]>
+) => {
+  const promises = [];
+  for (const ticker of coinTickers) {
+    console.log("📰 Scraping news articles for", ticker);
+    for (const result of fetchedNewsArticles.get(ticker) || []) {
       const url = result.url.replace(
         /https:\/\/cryptopanic\.com\/news\/(\d+)\/.*/,
         "https://cryptopanic.com/news/click/$1/"
       );
-
-      const scrapperRes = await extract(url);
-
-      console.log("\n\n✨ Generating for: ", scrapperRes.title);
-      console.log("📰 GOT ARTICLE: ", scrapperRes.content);
-
-      console.time("⏱️ Summary generation");
-      const AIRes: AxiosResponse<BananaResponseData> = await axios.post(
-        "https://api.banana.dev/start/v4/",
-        {
-          apiKey: env.BANANA_API_KEY,
-          modelKey: env.BANANA_MODEL_KEY,
-          modelInputs: {
-            prompt: scrapperRes.content,
-          },
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+      promises.push(
+        (async () => {
+          const scrapperRes = await scrapArticle(url);
+          console.log("🗞️", result.title);
+          if (scrapperRes) {
+            return {
+              ticker,
+              title: result.title,
+              url: result.url,
+              rawContent: scrapperRes.content,
+              summary: scrapperRes.description,
+              image: scrapperRes.image,
+              createdAt: result.created_at,
+            };
+          }
+        })()
       );
-      console.timeEnd("⏱️ Summary generation");
-
-      const aiGeneratedContent =
-        AIRes.data.modelOutputs &&
-        AIRes.data.modelOutputs[0] &&
-        AIRes.data.modelOutputs[0][0]?.summary_text;
-
-      if (aiGeneratedContent)
-        console.log("🪵 GOT AI CONTENT: ", aiGeneratedContent);
-      else {
-        console.log("❌ Failed to generate summary", AIRes.data);
-      }
-
-      articles.push({
-        coinIds: result.currencies?.map((c) => c.code),
-        title: result.title,
-        url: result.url,
-        content: aiGeneratedContent || scrapperRes.description,
-        image: scrapperRes.image,
-        createdAt: result.created_at,
-      });
-    } catch (e) {
-      console.log("🚨 ERROR: ", e);
     }
   }
-
-  return articles;
+  return Promise.all(promises);
 };
