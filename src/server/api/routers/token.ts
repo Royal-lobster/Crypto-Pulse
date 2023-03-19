5ee8aaa23cdb7bf63ce75c484f6871c674058716
Srujan Gurram <srujangs8@gmail.com>
2023-03-19 21:01:28 +0530

    Check if user has enough HiIQ before adding a new token to their account, and throw an error if they don't. Also add the token to the user's account if they have enough HiIQ and token count limits have not been exceeded

--- src/server/api/routers/token.ts
+++ src/server/api/routers/token.ts
@@ -16,6 +16,46 @@ export const tokenRouter = createTRPCRouter({
     .mutation(async ({ input, ctx }) => {
       const { tokenId, tickerId, tokenImg, tokenName } = input;
 
+      // ================================
+      // Check if user has enough HiIQ to add more tokens
+      // ================================
+
+      const user = await ctx.prisma.user.findUnique({
+        where: { id: ctx.userAddress },
+        select: {
+          hiIQ: true,
+          _count: {
+            select: {
+              tokens: true,
+            },
+          },
+        },
+      });
+
+      // 3 tokens - free
+      // 5 tokens - greater than 10000 HiIQ
+      // 10 tokens - greater than 500000 HiIQ
+      // 50 tokens - greater than 1000000 HiIQ
+
+      const userHiIQ = user?.hiIQ || 0;
+      const userTokenCount = user?._count?.tokens || 0;
+      const nextUserTokenCount = userTokenCount + 1;
+
+      if (nextUserTokenCount > 3) {
+        if (nextUserTokenCount > 5 && userHiIQ < 10000) {
+          throw new Error("Not enough HiIQ");
+        }
+        if (nextUserTokenCount > 10 && userHiIQ < 500000) {
+          throw new Error("Not enough HiIQ");
+        }
+        if (nextUserTokenCount > 50 && userHiIQ < 1000000) {
+          throw new Error("Not enough HiIQ");
+        }
+      }
+
+      // ================================
+      // Add token to user
+      // ================================
       const token = await ctx.prisma.token.upsert({
         where: { id: tokenId },
         update: {},
