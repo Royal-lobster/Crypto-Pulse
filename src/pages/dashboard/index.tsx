e081449c44e3476dbc4bb17ee235217d9393dd33
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-20 12:57:26 +0100

    removed tabs

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -43,78 +43,75 @@ const Dashboard = () => {
 
   return (
     <>
-      <Tab.Group defaultIndex={0}>
-        <div className="relative z-10 mx-auto mt-10 w-full px-0 sm:px-8 md:mt-20 md:px-10 xl:max-w-7xl xl:px-0">
-          <h1 className="font-inter text-2xl font-black text-white sm:text-3xl md:text-5xl">
-            Briefing for {getCurrentDate(Date.now())}
-          </h1>
-          <p className="mt-5 w-full font-display text-base font-normal leading-6 text-white opacity-60 sm:w-3/4 md:mt-8 md:w-[60%] md:text-lg xl:w-[50%]">
-            Briefing for your subscribed favourite tokens. Make sure to bookmark
-            this page !
-          </p>
-        </div>
-        <div className="mx-0 mt-10 flex items-center gap-4 rounded-xl border border-[#434447] pl-8 pr-3 sm:mx-8 md:mx-10 lg:fixed lg:top-1/2 lg:right-[20px] lg:mt-0 lg:flex lg:-translate-y-1/2 lg:flex-col lg:justify-center lg:gap-0 lg:py-5 lg:px-0 lg:pr-0 xl:right-[50px] xl:mx-0">
-          <div className="flex flex-shrink-0 items-center justify-center gap-4 py-2.5  text-[#ffffff7a] lg:flex-col  lg:px-2.5 lg:pt-5">
-            <div className="self-center text-xs uppercase lg:rotate-[270deg]">
-              <p>GO TO</p>
-            </div>
-            <span className="hidden lg:block">
-              <ArrowDown />
-            </span>
+      <div className="relative z-10 mx-auto mt-10 w-full px-0 sm:px-8 md:mt-20 md:px-10 xl:max-w-7xl xl:px-0">
+        <h1 className="font-inter text-2xl font-black text-white sm:text-3xl md:text-5xl">
+          Briefing for {getCurrentDate(Date.now())}
+        </h1>
+        <p className="mt-5 w-full font-display text-base font-normal leading-6 text-white opacity-60 sm:w-3/4 md:mt-8 md:w-[60%] md:text-lg xl:w-[50%]">
+          Briefing for your subscribed favourite tokens. Make sure to bookmark
+          this page !
+        </p>
+      </div>
+      <div className="mx-0 mt-10 flex items-center gap-4 rounded-xl border border-[#434447] pl-8 pr-3 sm:mx-8 md:mx-10 lg:fixed lg:top-1/2 lg:right-[20px] lg:mt-0 lg:flex lg:-translate-y-1/2 lg:flex-col lg:justify-center lg:gap-0 lg:py-5 lg:px-0 lg:pr-0 xl:right-[50px] xl:mx-0">
+        <div className="flex flex-shrink-0 items-center justify-center gap-4 py-2.5  text-[#ffffff7a] lg:flex-col  lg:px-2.5 lg:pt-5">
+          <div className="self-center text-xs uppercase lg:rotate-[270deg]">
+            <p>GO TO</p>
           </div>
-          <div className="mt-3 hidden h-[1px] w-full flex-grow bg-[#434447] lg:block" />
-          <Tab.List>
-            <div className="flex justify-center gap-3 overflow-scroll border-l border-[#434447] py-2.5 pl-3  lg:flex-col lg:justify-start lg:overflow-hidden lg:border-l-0 lg:py-0 lg:px-2.5 lg:pl-0 lg:pt-4">
-              {userSubsribedTokens?.map((subsribedToken) => (
-                <Tab key={subsribedToken.id} className="flex-shrink-0">
-                  <Image
-                    src={subsribedToken.image}
-                    alt={subsribedToken.id}
-                    width={36}
-                    height={36}
-                    className="h-[36px] w-[36px] rounded-full"
-                  />
-                </Tab>
-              ))}
-            </div>
-          </Tab.List>
+          <span className="hidden lg:block">
+            <ArrowDown />
+          </span>
         </div>
-        <Tab.Panels>
-          {userSubsribedTokens?.map((subscribedToken, i) => {
-            return (
-              <Tab.Panel key={`${subscribedToken.id}-${i}`}>
-                <div className="relative z-10 mx-auto mt-10 flex items-center px-0 sm:px-8 md:px-10 lg:mt-20 lg:pr-[100px] xl:max-w-7xl xl:pr-[70px] xl:pl-0">
-                  <div className="flex gap-3 rounded-xl border border-[#434447] py-3 px-10 pl-4">
-                    <Image
-                      src={subscribedToken.image}
-                      alt={subscribedToken.name}
-                      width={48}
-                      height={48}
-                      className="h-[48px] w-[48px] rounded-full"
-                    />
-                    <div className="flex-grow items-center">
-                      <h1 className="font-display text-base font-medium text-white">
-                        {subscribedToken.name}
-                      </h1>
-                      <p className="mt-1 text-xs uppercase text-[#ffffff7a]">
-                        {subscribedToken.ticker}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="h-[1px] flex-grow bg-[#434447]" />
-                </div>
-                <DashboardMainContent
-                  tokenName={subscribedToken.name}
-                  tokenImage={subscribedToken.image}
-                  tokenId={subscribedToken.id}
-                  setIsOpen={setIsOpen}
-                  handleNewsClick={handleNewsClick}
+        <div className="mt-3 hidden h-[1px] w-full flex-grow bg-[#434447] lg:block" />
+
+        <div className="flex justify-center gap-3 overflow-scroll border-l border-[#434447] py-2.5 pl-3 lg:flex-col lg:justify-start lg:overflow-hidden lg:border-l-0 lg:py-0 lg:px-2.5 lg:pl-2.5 lg:pt-4">
+          {userSubsribedTokens?.map((subsribedToken) => (
+            <div key={subsribedToken.id} className="flex-shrink-0">
+              <Image
+                src={subsribedToken.image}
+                alt={subsribedToken.id}
+                width={36}
+                height={36}
+                className="h-[36px] w-[36px] rounded-full"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {userSubsribedTokens?.map((subscribedToken, i) => {
+        return (
+          <div key={`${subscribedToken.id}-${i}`}>
+            <div className="relative z-10 mx-auto mt-10 flex items-center px-0 sm:px-8 md:px-10 lg:mt-20 lg:pr-[100px] xl:max-w-7xl xl:pr-[70px] xl:pl-0">
+              <div className="flex gap-3 rounded-xl border border-[#434447] py-3 px-10 pl-4">
+                <Image
+                  src={subscribedToken.image}
+                  alt={subscribedToken.name}
+                  width={48}
+                  height={48}
+                  className="h-[48px] w-[48px] rounded-full"
                 />
-              </Tab.Panel>
-            );
-          })}
-        </Tab.Panels>
-      </Tab.Group>
+                <div className="flex-grow items-center">
+                  <h1 className="font-display text-base font-medium text-white">
+                    {subscribedToken.name}
+                  </h1>
+                  <p className="mt-1 text-xs uppercase text-[#ffffff7a]">
+                    {subscribedToken.ticker}
+                  </p>
+                </div>
+              </div>
+              <div className="h-[1px] flex-grow bg-[#434447]" />
+            </div>
+            <DashboardMainContent
+              tokenName={subscribedToken.name}
+              tokenImage={subscribedToken.image}
+              tokenId={subscribedToken.id}
+              setIsOpen={setIsOpen}
+              handleNewsClick={handleNewsClick}
+            />
+          </div>
+        );
+      })}
+
       {newsDetails && (
         <PopNewsCard
           isOpen={isOpen}
