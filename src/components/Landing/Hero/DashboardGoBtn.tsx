1120b9bff20f971a518129b00fc84f1862896a71
Srujan Gurram <srujangs8@gmail.com>
2023-03-20 07:05:02 +0530

    Add ArrowPathIcon and loading animation to DashboardGoBtn component in Landing Hero section

--- src/components/Landing/Hero/DashboardGoBtn.tsx
+++ src/components/Landing/Hero/DashboardGoBtn.tsx
@@ -1,3 +1,4 @@
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import { useIsMutating } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { useAccount } from "wagmi";
@@ -16,8 +17,9 @@ const DashBoardGoButton = () => {
           onClick={() => {
             router.push("/dashboard").catch((e) => console.log(e));
           }}
-          className="cursor-pointer rounded-xl bg-[#FF5CAA] py-2 px-8 font-display text-white data-[disabled]:bg-[#A44D76]"
+          className="flex cursor-pointer items-center gap-2 rounded-xl bg-accent py-2 px-8 font-display text-white data-[disabled]:cursor-wait data-[disabled]:bg-accent/25"
         >
+          {isFetching ? <ArrowPathIcon className="w-4 animate-spin" /> : <></>}
           Check the Area
         </button>
       ) : (
