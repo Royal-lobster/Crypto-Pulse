d577386efbb5723845f9594b6e38d27995a9fdb7
Srujan Gurram <srujangs8@gmail.com>
2023-03-21 06:52:58 +0530

    fixes previous date news

--- src/utils/isYesterdayRelativeTo.ts
+++ src/utils/isYesterdayRelativeTo.ts
@@ -1,7 +1,3 @@
 export function isYesterdayRelativeTo(date1: Date, date2: Date): boolean {
-  const oneDay = 24 * 60 * 60 * 1000; // milliseconds in one day
-  const diffInDays = Math.round(
-    Math.abs((date1.getTime() - date2.getTime()) / oneDay)
-  );
-  return diffInDays === 1 && date1 < date2;
+  return date1.getDate() === date2.getDate() - 1;
 }
